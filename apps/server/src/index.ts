f2aa9fae7868a4c887fe0b64ef46575be570ecae
lteawoo <lteawoo@naver.com>
2025-11-07 20:08:23 +0900

    init pnpm and hono

---
+++ apps/server/src/index.ts
@@ -0,0 +1,34 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+const port = 3000
+
+const server = serve({
+  fetch: app.fetch,
+  port
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
+
+// Graceful shutdown
+process.on('SIGINT', () => {
+  console.log('[SIGINT]Shutting down server...')
+  server.close()
+  process.exit(0)
+})
+process.on('SIGTERM', () => {
+  console.log('[SIGTERM]Shutting down server...')
+  server.close((err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
+})
