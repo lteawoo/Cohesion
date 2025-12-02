ad32d08df7f9ab4b77cb976eb9b602da3162e457
lteawoo <lteawoo@naver.com>
2025-12-03 01:00:12 +0900

    add dark and light mode

--- apps/frontend/src/components/layout/MainLayout/MainSider.tsx
+++ apps/frontend/src/components/layout/MainLayout/MainSider.tsx
@@ -1,40 +1,41 @@
 import { PlusOutlined } from "@ant-design/icons";
-import { Button, Layout, Menu, type MenuProps } from "antd";
+import { Button, Layout, Menu, theme, type MenuProps } from "antd";
 import type { ItemType } from "antd/es/menu/interface";
 
 const { Sider } = Layout;
 
 interface MainSiderProps {
-    spaceItems: ItemType[];
+  spaceItems: ItemType[];
 }
 
 export default function MainSider({ spaceItems }: MainSiderProps) {
+  const { token } = theme.useToken();
 
-    const spaceMenuItems: MenuProps['items'] = [
-        {
-            key: 'space',
-            label: (
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <span>Space</span>
-                    <Button
-                        type="link"
-                        icon={<PlusOutlined />}
-                        size="small"
-                    ></Button>
-                </div>
-            ),
-            type: 'group',
-            children: spaceItems
-        }
-    ];
+  const spaceMenuItems: MenuProps['items'] = [
+    {
+      key: 'space',
+      label: (
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <span>Space</span>
+          <Button
+            type="link"
+            icon={<PlusOutlined />}
+            size="small"
+          ></Button>
+        </div>
+      ),
+      type: 'group',
+      children: spaceItems
+    }
+  ];
 
-    return (
-        <Sider
-            style={{
-
-            }}
-        >
-            <Menu mode="inline" items={spaceMenuItems} />
-        </Sider>
-    );
+  return (
+    <Sider
+      style={{
+        background: token.colorBgContainer
+      }}
+    >
+      <Menu mode="inline" items={spaceMenuItems} />
+    </Sider>
+  );
 }
--- apps/frontend/src/components/layout/MainLayout/index.tsx
+++ apps/frontend/src/components/layout/MainLayout/index.tsx
@@ -1,4 +1,4 @@
-import { ConfigProvider, Layout, theme } from "antd";
+import { ConfigProvider, Layout, Switch, theme } from "antd";
 import { Outlet } from "react-router";
 import { MailOutlined } from "@ant-design/icons";
 import MainSider from "./MainSider";
@@ -9,45 +9,57 @@ const { Header, Content } = Layout;
 
 const items: ItemType[] = [
     { key: '1', icon: <MailOutlined />, label: 'My folder' },
-]
+];
+
+const PageLayout = ({ isDarkMode, onThemeChange }: { isDarkMode: boolean, onThemeChange: (checked: boolean) => void }) => {
+  const { token } = theme.useToken();
+
+  return (
+    <Layout
+      style={{
+          display: 'flex',
+          minHeight: '100vh',
+          overflow: 'hidden'
+      }}
+    >
+      <Header
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          background: token.colorBgContainer
+        }}
+      >
+        <div style={{ color: token.colorText, fontSize: '20px' }}>
+            Cohesion
+        </div>
+        <Switch checked={isDarkMode} onChange={onThemeChange} checkedChildren="Dark" unCheckedChildren="Light" />
+      </Header>
+      <Layout>
+          <MainSider spaceItems={items} />
+
+          <Content>
+              <main style={{ flex: 1, overflowY: 'auto' }}>
+                  <Outlet />
+              </main>
+          </Content>
+      </Layout>
+    </Layout>
+  );
+}
 
 export default function MainLayout() {
     const [isDarkMode, setIsDarkMode] = useState(true);
 
-    const { token } = theme.useToken();
+    const handleThemeChange = (checked: boolean) => {
+        setIsDarkMode(checked);
+    };
 
     const currentAlgorithm = isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm;
 
-    console.log(currentAlgorithm)
-
     return (
         <ConfigProvider theme={{ algorithm: currentAlgorithm }}>
-            <Layout
-                style={{
-                    display: 'flex',
-                    minHeight: '100vh',
-                    overflow: 'hidden'
-                }}
-            >
-                <Header
-                    style={{
-                        background: token.colorBgContainer
-                    }}
-                >
-                    <div style={{ color: token.colorText, fontSize: '20px' }}>
-                        Cohesion
-                    </div>
-                </Header>
-                <Layout>
-                    <MainSider spaceItems={items} />
-
-                    <Content>
-                        <main style={{ flex: 1, overflowY: 'auto' }}>
-                            <Outlet />
-                        </main>
-                    </Content>
-                </Layout>
-            </Layout>
+          <PageLayout isDarkMode={isDarkMode} onThemeChange={handleThemeChange} />
         </ConfigProvider>
     )
 }
