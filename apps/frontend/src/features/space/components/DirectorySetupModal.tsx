eb8437453e7b32a54d6fb58fbeeeaaa97132411c
lteawoo <lteawoo@naver.com>
2025-12-07 20:30:00 +0900

    work:

--- apps/frontend/src/features/space/components/DirectorySetupModal.tsx
+++ apps/frontend/src/features/space/components/DirectorySetupModal.tsx
@@ -1,37 +1,60 @@
-import { Modal, Form, Input } from "antd";
-import { useEffect } from "react";
+import { Modal, } from "antd";
+import { useEffect, useState } from "react";
+import type { FileInfo } from "../types";
+import { HomeFilled } from "@ant-design/icons";
+import { RiHardDrive3Fill } from "react-icons/ri";
 
 export default function DirectorySetupModal({ isOpen, onClose } : { isOpen: boolean, onClose: () => void}) {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [baseDirectories, setBaseDirectories] = useState<FileInfo[]>([]);
 
   useEffect(() => {
-    if (!isOpen) {
-      // Reset form or perform cleanup
+    const fetchBaseDir = async () => {
+      try {
+        const response = await fetch('/api/browse/base-directories');
+        const data = await response.json();
+        console.log('Base Directories:', data);
+
+        if (!response.ok) {
+          throw new Error(data.message || 'Failed to fetch base directories');
+        }
+
+        setBaseDirectories(data);
+      } catch (error) {
+        console.error('Error fetching base directories:', error);
+      } finally {
+        setLoading(false);
+      }
     }
-  }, [isOpen]);
+    fetchBaseDir();
+  }, []);
 
   return (
     <Modal
         title="Space 등록"
         open={isOpen}
         onCancel={onClose}
-      >
-      <Form>
-        <Form.Item
-          name="spaceName"
-          label="Name"
-          rules={[{ required: true }]}
-        >
-          <Input placeholder="Ex) Movie" />
-        </Form.Item>
-
-        <Form.Item
-          name="spacePath"
-          label="Space Path"
-          rules={[{ required: true }]}
-        >
-          <Input placeholder="Ex) 폴더 경로 설정" />
-        </Form.Item>
-      </Form>
+        width= {{
+          xs: '90%',
+          md: '70%',
+          lg: '60%',
+        }}>
+      <div style={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
+        {/* user folder and disk */}
+        <div style={{ flex: 0.7 }}>
+          {baseDirectories.map((dir: FileInfo) => {
+            if (dir.name === 'Home') {
+              return <div key={dir.path}><HomeFilled /> Home</div>
+            } else {
+              return <div key={dir.path}><RiHardDrive3Fill /> {dir.name}</div>
+            }
+          })}
+        </div>
+        {/* directory list */}
+        <div style={{ flex: 1 }}  >
+          directory list
+        </div>
+      </div>
     </Modal>
   );
 }
